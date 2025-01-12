c3b661c94b05450ccf854ea3f644cbea4ce7a94c
abko <abko121115@gmail.com>
2025-01-12 13:19:34 +0900

    이런 방법이...

--- solved-ac/Q1018.java
+++ solved-ac/Q1018.java
@@ -18,18 +18,21 @@ public class Q1018 {
         for(int startX = 0; startX+7 < board.length; startX++) {
             for(int startY = 0; startY+7 < board[startX].length; startY++) {
                 int sumA = 0;
-                int sumB = 0;
+                // int sumB = 0;
                 for(int x = 0; x < 8; x++) {
                     for(int y = 0; y < 8; y++) {
                         if(!board[startX+x][startY+y].equals(pattern[x%2][y%2])) {
                             sumA++;
                         }
-                        if(!board[startX+x][startY+y].equals(pattern[x%2][(y+1)%2])) {
-                            sumB++;
-                        }
+                        // if(!board[startX+x][startY+y].equals(pattern[x%2][(y+1)%2])) {
+                        //     sumB++;
+                        // }
                     }
                 }
-                min = Math.min(min, Math.min(sumA, sumB));
+                // sumA는 하얀색으로 시작하는 경우
+                // 그 반대의 경우는 단순히 전체 칸에서 sumA를 빼면 된다.
+                // 그 이유는 두 경우는 서로 대칭이기 때문이다.
+                min = Math.min(min, Math.min(sumA, 64-sumA));
             }
         }
         
