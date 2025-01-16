32329b6190776d3b4d688416a6edee0e36f03eb7
abko <abko121115@gmail.com>
2025-01-16 10:43:48 +0900

    "같은 숫자는 싫어" 클리어. Queue와 Deque의 구현체인 LinkedList에 대해서 더 공부 해둬야겠음.

---
+++ programmers/Q12906.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.offer(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(queue.peekLast() == arr[i]) {
+                continue;
+            }
+            queue.offer(arr[i]);
+        }
+        return queue.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+
+public class Q12906 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = new int[]{1,1,3,3,0,1,1};
+        System.out.print(Arrays.toString(sol.solution(arr)));
+    }
+}
